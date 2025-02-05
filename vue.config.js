274d5b4d34f2bbd4c7a86c53462a8a2d114dd142
Felipe Jimenez Lozano <fjimenezlozano@gmail.com>
2025-02-05 10:18:43 -0500

    Update

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  outputDir: 'dist', // Carpeta donde se generará la librería
+  configureWebpack: {
+    output: {
+      library: 'vue3-components', // Nombre global de la librería (para UMD)
+      libraryTarget: 'umd', // Compatible con varios entornos (ES, CommonJS, Global)
+      umdNamedDefine: true,
+    },
+    externals: {
+      vue: 'Vue', // Evita incluir Vue en el bundle (el proyecto que la use debe instalarlo)
+    },
+  },
+  transpileDependencies: true,
+});
